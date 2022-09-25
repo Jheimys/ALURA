e12e9b74cf03f17ab86443c33c4944a14831d063
Jheimys Bassani <jheimysbb@gmail.com>
2022-09-25 19:19:01 -0300

    Metodos com arrays

---
+++ Javascript/1_Arrays/4-slice/index.js
@@ -0,0 +1,9 @@
+const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const num1 = num.slice(0, num.length/2)
+
+const num2 = num.slice(num.length/2)
+
+console.log('Números:', num)
+console.log('Números 1:', num1)
+console.log('Números 2:', num2)
