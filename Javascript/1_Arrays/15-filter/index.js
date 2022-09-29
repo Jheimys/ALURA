68f72ffee72f880c78c6a1d75829f3e10cd1fa60
Jheimys Bassani <jheimysbb@gmail.com>
2022-09-29 07:11:50 -0300

    subindo arquivos da aula de array

--- Javascript/1_Arrays/15-filter/index.js
+++ Javascript/1_Arrays/15-filter/index.js
@@ -1,6 +1,6 @@
 const nomes = ['Ana', 'Marcos', 'Maria', 'Mauro']
 const notas = [7, 4.5, 8, 7.5]
 
-const reprovados = nomes.filter((nome, indice) => notas[indice] < 5 )
+const reprovados = nomes.filter((nome, elemento) => notas[elemento] < 5 )
 
 console.log(reprovados)
