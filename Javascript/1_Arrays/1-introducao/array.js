99989f7d86e137862ce8b9b3c371f10a9cb44226
Jheimys Bassani <jheimysbb@gmail.com>
2022-09-30 08:21:39 -0300

    revisao dos codigos

--- Javascript/1_Arrays/1-introducao/array.js
+++ Javascript/1_Arrays/1-introducao/array.js
@@ -12,3 +12,7 @@ const notas = [10, 6.5, 8, 7.5]
 let media = (notas[0] + notas[1] + notas[2] + notas[3])/notas.length
 
 console.log(media)
+
+//EXEMPLO 02:
+const produtos = []
+console.log(typeof(produtos))
