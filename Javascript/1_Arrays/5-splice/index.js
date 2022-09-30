70207a93a887ae111f346102fd32afaa098b9c45
Jheimys Bassani <jheimysbb@gmail.com>
2022-09-30 09:53:23 -0300

    revisao

--- Javascript/1_Arrays/5-splice/index.js
+++ Javascript/1_Arrays/5-splice/index.js
@@ -1,12 +1,12 @@
 const nomes =['Ana', 'Maria', 'Caio', 'Pedro', 'Artu', 'João', 'Marcos', 'Leo']
+console.log("Nomes:", nomes)
 
-//slice(x,y,z) 
+//slice(x,y,z) --> "emendar" OBS: altera o vetor original
 //x é de onde quero começar a excluir
 //y é quantos quero excluir
 //z é o que quero add
 nomes.splice(2, 3, 'José')
 
-console.log("Nomes:", nomes)
 console.log("Nomes 1:", nomes)
 
 nomes.splice(0, 0, 'Pedro', 'Elisa')
