7f0bc16cf22239e9a3564c824b0f10d92dab9bc4
Jheimys Bassani <jheimysbb@gmail.com>
2022-11-20 16:52:45 -0300

    ex 9

--- Javascript/Livro/cap06/9_exercício/js/script.js
+++ Javascript/Livro/cap06/9_exercício/js/script.js
@@ -2,16 +2,21 @@ const form = document.querySelector('form')
 const resp = document.querySelector('pre')
 
 const criancas = []
+console.log('Vetor de criancas:', criancas)
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
     const nome = form.inNome.value
     const idade = Number(form.inIdade.value)
     criancas.push({nome, idade})
+    console.log('Vetor de criancas dentro form:', criancas)
     form.reset()
     form.inNome.focus()
-    form.btListar.dispatchEvent(new Event('click'))
+
+    // Dispara o evento de click automático no bt Listar.  
+    form.btListar.dispatchEvent(new Event('click')) 
 })
+
 
 form.btListar.addEventListener('click', () => {
     if(criancas.length === 0){
@@ -40,37 +45,32 @@ form.btResumir.addEventListener('click', () => {
     copia.sort((a,b) => a.idade - b.idade) //Ordena o vetor copia pela idade
     let aux = copia[0].idade //Menor idade do vetor ordenado
 
-    console.log(copia)
+    console.log('Vetor copia:', copia)
     //console.log(aux)
 
     let resumo = ''
     let nomes = []
+    console.log('Vetor de nomes:',nomes)
 
     copia.forEach((crianca) => {
         const {nome, idade} = crianca
 
         if(idade == aux){
-
-            nomes.push(nome)
-            
-
+            nomes.push(nome)   
         } else {
-            console.log('aux:', aux)
             resumo += `${aux} ano(s): ${nomes.length} criança(s) -`
             resumo += `${((nomes.length / copia.length) * 100).toFixed(2)}%\n`
             resumo += `(${nomes.join(', ')})\n\n`
-            //console.log(resumo)
+            console.log('lista resumo:', resumo)
             
             aux = idade
 
             nomes = []
             nomes.push(nome)
-            
+            console.log('Vetor nome dentro do else:', nomes)   
         }
-
     })
 
-    
     resumo += `${aux} ano(s): ${nomes.length} criança(s) -`
     resumo += `${((nomes.length / copia.length) * 100).toFixed(2)}%\n`
     resumo += `(${nomes.join(', ')})\n\n`
@@ -78,5 +78,5 @@ form.btResumir.addEventListener('click', () => {
   
 })
 
-console.log(criancas)
+
 
