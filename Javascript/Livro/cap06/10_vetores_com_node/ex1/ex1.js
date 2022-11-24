774835e9c1e7774d70f1f78736baad88a1de1ceb
Jheimys Bassani <jheimysbb@gmail.com>
2022-11-24 11:10:01 -0300

    lacos de repeticao

---
+++ Javascript/Livro/cap06/10_vetores_com_node/ex1/ex1.js
@@ -0,0 +1,7 @@
+import promptSync from "prompt-sync";
+import chalk from "chalk";
+const prompt = promptSync()
+
+console.log(chalk.green("Informe os alunos. Apos, digite 'fim' no nome para sair"))
+const alunos = []
+
