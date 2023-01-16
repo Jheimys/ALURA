c067e82904baa59f4f97127393e957af885474e0
Jheimys Bassani <jheimysbb@gmail.com>
2023-01-16 15:15:15 -0300

    estudando node express

--- Nodejs_express/Mess/src/app.js
+++ Nodejs_express/Mess/src/app.js
@@ -1,18 +1,23 @@
+// 1° --> importar express
+// 2° --> Chamar o express
+// 3° --> Criar uma rota
+// 4° --> exportar app 
 import express from "express"
 
 const app = express()
 
-
-//Habilita o uso do json nos métodos.
-app.use(express.json())
-
 const livros = [
-    {id: 1, "titulo": "senhor dos Aneis"},
-    {id: 2, "titulo": "O Hobbit"}
+    {id: 1, "titulo": "Conjuntos e funções"},
+    {id: 2, "titulo": "Logaritmos"},
+    {id: 3, "titulo": "Tigonometria"}
 ]
 
+//Para ler arquivos json
+app.use(express.json())
+
+//Rotas
 app.get('/', (req, res) => {
-    res.status(200).send('Curso de Node')
+    res.status(200).send('Fundamentos da Matemática Elementar')
 })
 
 app.get('/livros', (req, res) => {
@@ -21,29 +26,31 @@ app.get('/livros', (req, res) => {
 
 app.get('/livros/:id', (req, res) => {
     let index = buscaLivro(req.params.id)
-    res.json(livros[index])
+    res.json(livros[index]) 
 })
 
 app.post('/livros', (req, res) => {
     livros.push(req.body)
-    res.status(201).send('O livro foi cadastrado com sucesso.')
+    res.status(201).send('O livro foi cadastrado com  sucesso.')
 })
 
 app.put('/livros/:id', (req, res) => {
     let index = buscaLivro(req.params.id)
     livros[index].titulo = req.body.titulo
-    res.json(livros)
+    res.json(livros) 
 })
 
 app.delete('/livros/:id', (req, res) => {
     let {id} = req.params
     let index = buscaLivro(id)
     livros.splice(index, 1)
-    res.send(`Livro ${id} removido com sucesso`)
+    res.send(`O livro ${id} foi removido com sucesso.`)
 })
 
 function buscaLivro(id) {
     return livros.findIndex(livro => livro.id == id)
 }
+
+
 
 export default app
